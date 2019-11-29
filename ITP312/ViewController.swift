a7b938043e02bdc6ffcf84756ab5f74030ed80d2
Han Jiyao <hanjiyao0@gmail.com>
2019-11-29 10:40:05 +0800

    Initial Commit

---
+++ ITP312/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ITP312
+//
+//  Created by ITP312 on 29/11/19.
+//  Copyright © 2019 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
